3b617ae85e83c6e2e23da8bccc712877bf8f2885
ola mohamed <olamohamedola276@gmail.com>
2023-09-15 04:38:19 -0700

    -printf begg

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ *_printf - printf fun
+ *@format :
+ *@... : 
+ *Return :
+ */
+int _printf(const char *format, ...)
+{
+va_list arg;
+int in = 0;
+int c = 0;
+
+va_start(arg, format);
+}
