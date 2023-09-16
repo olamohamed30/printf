2715e9a39c18740bee15473f06b688e3f582c77a
ola mohamed <olamohamedola276@gmail.com>
2023-09-16 05:35:59 -0700

    printf structure

--- _printf.c
+++ _printf.c
@@ -1,17 +1,79 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h> 
 /**
- *_printf - printf fun
- *@format :
- *@... : 
- *Return :
- */
+ *  * _printf- printf made by duaa and ola
+ *   * @format: arg that will be added to fun
+ *    * @... :
+ *     *Return 
+ *      **/
+ 
+
 int _printf(const char *format, ...)
 {
-va_list arg;
-int in = 0;
-int c = 0;
+	/*تعريف الحجات*/
+	va_list arg;
+	int creturn = 0;
 
-va_start(arg, format);
+	va_start(arg, format);
+	/*لوب */
+	for (; *format; format++)
+	{
+		/*لو هي مش السبسيفير  اطبع وخلاص */
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			creturn ++;
+		}
+		else
+			/*  لو هي هنعمل ال سوتش علي كل حاله */
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					/*حاله الحرف هنطبع عادي ونزود الكونت */
+					char c = va_arg(arg, int);
+					write(1, &c, 1);
+					creturn++;
+					break;
+				case 's':
+					{
+						/*بما انه نص نفس شغل ال char بس ب لوب */
+						char *string = va_arg(arg, char*);
+						while (*string)
+							/*لما نوصل  null terminator */
+						{
+							write(1, string, 1);
+							creturn++;
+							string++;
+						}
+						break;
+					}
+				case '%':
+					{
+						/*هنطبع عادي جدا ونزود الكونت*/
+						write(1, format, 1);
+						creturn++;
+						break;
+					}
+				case 'd':
+				case 'i':
+					{
+						/* duaa part = fun like itoa */
+						break;
+					}
+
+				default:
+					{
+						/*ola still under work */
+						break;
+					}
+
+			}
+		}
+	}
+
+	va_end(arg);
+
+	return creturn;
 }
