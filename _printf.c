08e76916afdbf536c6663bbb1e782dae77b3fa00
Duaa <dabulfetouh@gmail.com>
2023-09-16 06:09:06 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,81 +1,16 @@
 #include "main.h"
 #include <unistd.h> 
 /**
-<<<<<<< HEAD
  *_printf - printf fun
  *@format :
  *@... :
  *Return :
-=======
  *  * _printf- printf made by duaa and ola
  *   * @format: arg that will be added to fun
  *    * @... :
- *     *Return 
->>>>>>> d08cd5a3a915bd030de44bcd7e902ddc94f4111d
  */
- 
-
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-	va_list arg;
-	int in = 0;
-	int c = 0;
-
-	va_start(arg, format);
-
-	    
-	while (format && format[in])
-	{
-		if (format[in] == '%')
-		{
-			in++;
-			switch (format[in])
-			{
-				case 'c':
-					{
-						char c = va_arg(arg, int);
-						write(1, &c, 1);
-						c++;
-						break;
-					}
-				case 's':
-					{
-						char *str = va_arg(arg, char *);
-						int k = 0;
-						while (str && str[k])
-						{
-							write(1, &str[k], 1);
-							c++;
-							k++;
-						}
-						break;
-					}
-				case '%':
-					{
-						int k = 0;
-						write(1, &format[k], 1);
-						c++;
-						break;
-					}
-				default:
-					{
-						break;
-					}
-			}
-		}
-		else
-		{
-			write(1, &format[in], 1);
-			c++;
-		}
-		in++;
-	}
-
-	va_end(arg);
-
-	return c;
-=======
 	/*تعريف الحجات*/
 	va_list arg;
 	int creturn = 0;
@@ -142,6 +77,4 @@ int _printf(const char *format, ...)
 	va_end(arg);
 
 	return creturn;
->>>>>>> d08cd5a3a915bd030de44bcd7e902ddc94f4111d
 }
-
