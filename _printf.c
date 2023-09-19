c15e14f52b2c2bd1f792e163e00d5476e62075bf
Duaa <dabulfetouh@gmail.com>
2023-09-19 04:29:54 -0700

    buffer 1024

--- _printf.c
+++ _printf.c
@@ -76,6 +76,28 @@ void intduaa(va_list *arg, int *c)
 	}
 }
 /**
+ * rev_st - handles the %r specifer
+ * @arg: argument parameter handeling
+ * @c: counting paratmeter handling
+ * l - lenth of string
+ * y: prints string in reverse
+ * @st - retrives the string argument
+ */
+void rev_st(va_list *arg, int *c)
+{
+	char *st = va_arg(*arg, char *);
+	int l = 0;
+	int y;
+
+	while (st[l] != '\0')
+		l++;
+	for (y = l - 1; y >= 0; y--)
+	{
+		write(1, &st[y], 1);
+		(*c)++;
+	}
+}
+/**
  * allspec - know the specifier then go to its fun
  * @format: Format string
  * @arg: Arg list
@@ -99,10 +121,13 @@ int allspec(const char *format, va_list *arg, int *c)
 			return (2);
 		case 'i':
 			intduaa(arg, c);
-                        return (2);
+                        return (4);
 		case 'd':
 			intduaa(arg, c);
-			return (2);
+			return (4);
+		case 'r':
+			rev_st(arg, c);
+			return(2);
 
 		default:
 			return (1);
